a53485eb30c3932b3058c0228507c4b7ce2cd34f
Yuvrajsinh Vala <yuvrajsinh.vala@letsnurture.com>
2017-03-10 16:28:03 +0530

    second commit

--- Example/YVPods/ViewController.swift
+++ Example/YVPods/ViewController.swift
@@ -1,10 +1,7 @@
-//
-//  ViewController.swift
-//  YVPods
-//
-//  Created by yuvrajsinh777@gmail.com on 03/09/2017.
-//  Copyright (c) 2017 yuvrajsinh777@gmail.com. All rights reserved.
-//
+            
+            
+            
+            
 
 import UIKit
 
@@ -12,9 +9,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
     }
-
+    //kljfhdk
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
